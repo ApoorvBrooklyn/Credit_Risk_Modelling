9b7cb540e42275aec90c9d56854c3140db960025
ApoorvBrooklyn <apoorobot21@gmail.com>
2024-07-02 22:11:27 +0530

    Initial Commit

---
+++ Model.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+train = pd.read_csv(r'Dataset\cs-training.csv')
+test = pd.read_csv(r'Dataset\cs-test.csv')
+
+
+
+
+
